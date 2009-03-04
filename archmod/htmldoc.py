67f60c6cfdada2548f8a9af63c78b3e19b75a3ef
Basil Shubin <basil.shubin@gmail.com>
2009-03-04 09:03:17 +0000

    Small code formatting

--- archmod/htmldoc.py
+++ archmod/htmldoc.py
@@ -9,18 +9,18 @@ import subprocess
 
 
 def htmldoc(input, cmd, toclevels, output):
-    """CHM to other format converter
+	"""CHM to other format converter
 
-    input - list of input html files
-    cmd - htmldoc command with options
-    toclevels - ToC levels as htmldoc option
-    output - output file (single html, ps, pdf and etc)
-    """
-    command = cmd
-    if toclevels is not None:
-            command += (' --toclevels %s' % (toclevels))
-    command += (' --outfile %s' % (output))
-    files = string.join(input)
-    if len(files):
-        command = command + ' ' + files
-        subprocess.call(command, shell=True)
+	input - list of input html files
+	cmd - htmldoc command with options
+	toclevels - ToC levels as htmldoc option
+	output - output file (single html, ps, pdf and etc)
+	"""
+	command = cmd
+	if toclevels is not None:
+		command += (' --toclevels %s' % (toclevels))
+	command += (' --outfile %s' % (output))
+	files = string.join(input)
+	if len(files):
+		command = command + ' ' + files
+		subprocess.call(command, shell=True)
