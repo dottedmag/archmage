da9d8e066fb2224194cb1d054e9750140d1db29b
Basil Shubin <basil.shubin@gmail.com>
2009-04-18 03:55:25 +0000

    Initial import

---
+++ archmod/HTMLTags.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+"""Class of all HTML tags"""
+
+class HTMLTags(object):
+
+	tags = [
+		'ADDRESS',
+		'APPLET',
+		'AREA',
+		'A',
+		'BASE',
+		'BASEFONT',
+		'BIG',
+		'BLOCKQUOTE',
+		'BODY',
+		'BR',
+		'B',
+		'CAPTION',
+		'CENTER',
+		'CITE',
+		'CODE',
+		'DD',
+		'DFN',
+		'DIR',
+		'DIV',
+		'DL',
+		'DT',
+		'EM',
+		'FONT',
+		'FORM',
+		'H1',
+		'H2',
+		'H3',
+		'H4',
+		'H5',
+		'H6',
+		'HEAD',
+		'HR',
+		'HTML',
+		'IMG',
+		'INPUT',
+		'ISINDEX',
+		'I',
+		'KBD',
+		'LINK',
+		'LI',
+		'MAP',
+		'MENU',
+		'META',
+		'OL',
+		'OPTION',
+		'PARAM',
+		'PRE',
+		'P',
+		'SAMP',
+		'SCRIPT',
+		'SELECT',
+		'SMALL',
+		'STRIKE',
+		'STRONG',
+		'STYLE',
+		'SUB',
+		'SUP',
+		'TABLE',
+		'TD',
+		'TEXTAREA',
+		'TH',
+		'TITLE',
+		'TR',
+		'TT',
+		'UL',
+		'U',
+		'VAR'
+	]
+	
+	def __getattr__(self, value):
+		try:
+			index = self.tags.index(value.upper())
+		except:
+			index = None
+		if index:
+			if value.islower():
+				return self.tags[index].lower()
+			else:
+				return self.tags[index]
+		raise AttributeError(value)
