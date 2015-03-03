76f3a0142be6861cc3a41a034367042a1aff2e9a
Mikhail Gusarov <dottedmag@dottedmag.net>
2015-03-03 22:26:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name = "archmage",
     fullname = "arCHMage",
     version = "0.2.4",
-    description = "CHM(Compiled HTML) Decompressor",
+    description = "CHM decompressor",
     maintainer = "Basil Shubin",
     maintainer_email = "bashu@users.sourceforge.net",
     author = "Eugeny Korekin",
@@ -14,7 +14,7 @@ setup(
     url = "archmage.sf.net",
     license = "GPL",
     keywords = ["chm", "HTML Help", "Compiled HTML", "Compressed HTML"],
-    long_description = "arCHMage is an extensible reader and decompiler for files in the CHM format. arCHMage is written in the Python programming language and uses PyCHM - python bindings for CHMLIB from GnoCHM project.",
+    long_description = "arCHMage is a reader and decompressor for CHM format",
     packages=find_packages(),
     entry_points={
         'console_scripts': ['archmage = archmod.cli:main'],
