439f3831b201e7493e0152cf4640af1d8c93e6cd
Mikhail Gusarov <dottedmag@dottedmag.net>
2015-03-04 10:47:42 +0100

    Clean up and modernize setup.py

--- setup.py
+++ setup.py
@@ -3,20 +3,33 @@
 from setuptools import setup, find_packages
 import version
 
+long_desc='''arCHMage is a reader and decompressor for CHM format'''
+
+classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Console',
+    'Environment :: Web Environment',
+    'Intended Audience :: End Users/Desktop',
+    'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+    'Topic :: Text Processing :: Filters',
+]
+
 setup(
-    name = "archmage",
-    fullname = "arCHMage",
+    name='archmage',
     version=version.getVersion(),
-    description = "CHM decompressor",
-    maintainer = "Basil Shubin",
-    maintainer_email = "bashu@users.sourceforge.net",
-    author = "Eugeny Korekin",
-    author_email = "az@ftc.ru",
-    url = "archmage.sf.net",
-    license = "GPLv2+",
-    keywords = ["chm", "HTML Help", "Compiled HTML", "Compressed HTML"],
-    long_description = "arCHMage is a reader and decompressor for CHM format",
+    description='CHM decompressor',
+    maintainer='Mikhail Gusarov',
+    maintainer_email='dottedmag@dottedmag.net',
+    url='https://github.com/dottedmag/archmage',
+    license='GPLv2+',
+    keywords=['chm', 'HTML Help', 'Compiled HTML', 'Compressed HTML'],
+    classifiers=classifiers,
+    long_description=long_desc,
     packages=find_packages(),
+    install_requires=[
+        'pychm',
+        'BeautifulSoup',
+    ],
     entry_points={
         'console_scripts': ['archmage = archmod.cli:main'],
     },
